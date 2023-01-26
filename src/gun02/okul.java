2b86ac4d11dda1cb1725d71f66f0f7ffb51b68ad
Casper <sevdaerdem1@windowslive.com>
2023-01-26 19:39:42 +0300

    Ilk task 2

--- src/gun02/okul.java
+++ src/gun02/okul.java
@@ -4,6 +4,7 @@ public class okul {
     public static void main(String[] args) {
         System.out.println("selamlar");
         System.out.println("selamlar ben");
+        System.out.println("geldik  ");
 
 
     }
